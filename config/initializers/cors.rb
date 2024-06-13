ff9b4e879e57ac14a4ac0ee5e84745f2cb7726a0
KmxNnCCZ <mmmm48698062@icloud.com>
2024-06-13 23:08:46 +0900

    corsのurl追加

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000"
+    origins "localhost:3000", "https://rate-footballer-frontend-react-3d4e150d2790.herokuapp.com"
 
     resource "*",
       headers: :any,
