e50ff2b673e88d45372cd137b824b5d666d96da7
KmxNnCCZ <mmmm48698062@icloud.com>
2024-06-13 22:55:37 +0900

    Fix typo in Match model

--- app/models/match.rb
+++ app/models/match.rb
@@ -18,9 +18,9 @@ class Match < ApplicationRecord
   belongs_to :home_team, class_name: 'Team', foreign_key: 'home_team_id'
   belongs_to :away_team, class_name: 'Team', foreign_key: 'away_team_id'
 
-  validates match_api_id, presence: true, numericality: { only_integer: true }
-  validate utcDate, presence: true
-  validate season, presence: true, length: { is: 5 }
-  validate home_team_scoree, presence: true, numericality: { only_integer: true }
-  validate away_team_score, presence: true, numericality: { only_integer: true }
+  validates :match_api_id, presence: true, numericality: { only_integer: true }
+  validates :utcDate, presence: true
+  validates :season, presence: true, length: { is: 5 }
+  validates :home_team_scoree, presence: true, numericality: { only_integer: true }
+  validates :away_team_score, presence: true, numericality: { only_integer: true }
 end
