fd9a95eb8235242e6798a7e5143b257b96da2817
ああああああ <mmmm48698062@icloud.com>
2024-06-16 19:31:56 +0900

    corsoriginのurlの変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "https://rate-footballer-frontend-react-877c7a3f3d30.herokuapp.com/login"
+    origins "localhost:3000", "https://rate-footballer-frontend-react-877c7a3f3d30.herokuapp.com"
 
     resource "*",
       headers: :any,
