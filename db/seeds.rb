8f5f3b66cbf81b97afe9880a27c59d74bef3be3d
ああああああ <mmmm48698062@icloud.com>
2024-06-19 18:04:29 +0900

    seedするファイルの変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-# load Rails.root.join('db', 'seeds', 'teams.rb')
-# load Rails.root.join('db', 'seeds', 'players.rb')
-load Rails.root.join('db', 'seeds', 'matches.rb')
+load Rails.root.join('db', 'seeds', 'teams.rb')
+load Rails.root.join('db', 'seeds', 'players.rb')
+# load Rails.root.join('db', 'seeds', 'matches.rb')
