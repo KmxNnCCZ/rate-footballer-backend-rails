262a3a08e417043f8260c5a8c9f90186de296bfd
ああああああ <mmmm48698062@icloud.com>
2024-06-28 21:07:23 +0900

    不要なseedファイルを読み込まれないようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-load Rails.root.join('db', 'seeds', 'teams.rb')
+# load Rails.root.join('db', 'seeds', 'teams.rb')
 p "team完了"
 sleep(1)
 load Rails.root.join('db', 'seeds', 'players.rb')
 p "player完了"
 sleep(1)
-load Rails.root.join('db', 'seeds', 'matches.rb')
+# load Rails.root.join('db', 'seeds', 'matches.rb')
 p "match完了"
